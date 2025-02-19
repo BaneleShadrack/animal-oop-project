156cbb2f387a4c0778d5200dc26e313555bad3c8
BaneleShadrack <banele.shadrack@gmail.com>
2025-02-19 12:52:58 +0200

    A working test and practice file

--- src/animals.js
+++ src/animals.js
@@ -34,59 +34,59 @@ class Home {
     this.sounds = [];
   }
 
-  adoptPet(Animal) {
-    if (this.adoptedPets.indexOf(Animal) === -1) {
-      this.adoptedPets.push(Animal);
+  adoptPet(pet) {
+    if (this.adoptedPets.indexOf(pet) === -1) {
+      this.adoptedPets.push(pet);
+      this.sounds.push(pet.sound());
       return this.adoptedPets.length;
     } else {
-      return "error message: You cannot adopt the same animal twice";
+      return `error message: You cannot adopt ${Animal.name} twice`;
     }
   }
 
-  /*makeAllSounds(){
-            this.sounds.push(Animal.sound());
-            return sounds;
-        }*/
+  makeAllSounds() {
+    return this.sounds;
+  }
 }
 
 console.log("------------------------1--------------------------------");
-/* let dog1 = new Dog();
-    let dog2 = new Dog("Simba")
-    
-    console.log(dog1.eat());   // returns 'Rax eats'
-    console.log(dog1.sound()); // returns 'Bark'
-    
-    console.log(dog2.eat());// returns 'Simba eats'
-    console.log(dog2.sound()); // returns 'Bark'
-    
-    let cat1 = new Cat();
-    let cat2 = new Cat("Smokey")
-    
-    console.log(cat1.eat());   // returns 'Stormy eats'
-    console.log(cat1.sound()); // returns 'Meow'
-    
-    console.log(cat2.eat());    // returns 'Smokey eats'
-    console.log(cat2.sound());  // returns 'Meow'*/
+let dog1 = new Dog();
+let dog2 = new Dog("Simba");
+
+console.log(dog1.eat()); // returns 'Rax eats'
+console.log(dog1.sound()); // returns 'Bark'
+
+console.log(dog2.eat()); // returns 'Simba eats'
+console.log(dog2.sound()); // returns 'Bark'
+
+let cat1 = new Cat();
+let cat2 = new Cat("Smokey");
+
+console.log(cat1.eat()); // returns 'Stormy eats'
+console.log(cat1.sound()); // returns 'Meow'
+
+console.log(cat2.eat()); // returns 'Smokey eats'
+console.log(cat2.sound()); // returns 'Meow'
 console.log("//////////////////////jj/////////////////////////////////");
 let home = new Home();
-let dog1 = new Dog();
-let dog2 = new Dog();
+//let dog1 = new Dog();
+//let dog2 = new Dog();
 let cat = new Cat();
 
-//home.makeAllSounds();// this returns an empty array
+console.log(home.makeAllSounds()); // this returns an empty array
 console.log(home.adoptPet(dog1)); // 1
-//home.makeAllSounds();
+console.log(home.makeAllSounds());
 // this returns:
 // ["Bark"]
 
 console.log(home.adoptPet(cat)); // 2
-//home.makeAllSounds();
+console.log(home.makeAllSounds());
 // this returns:
 // ["Bark", "Meow"]
 
 console.log(home.adoptPet(dog2)); // 3
 console.log("lllllllllllllllllllllllllllllllllll");
 console.log(home.adoptPet(dog2));
-//home.makeAllSounds();
+console.log(home.makeAllSounds());
 // this returns:
 // ["Bark", "Meow", "Bark"]
